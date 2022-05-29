826bb2f9298ef720606fe40db7b83c4c19d4e340
Vivek Gupta <vivekvikigupta@gmail.com>
2022-05-29 20:57:40 +0530

    medtore changes

---
+++ middlewares/medStore_func.js
@@ -0,0 +1,14 @@
+const medStore = require('../model/medStoreSchema')
+const bcrypt = require('bcrypt.js')
+
+
+
+const reg_medStore = (req, res)=>{
+
+}
+
+const login_medStore = (req, res)=>{
+
+}
+
+module.exports = { reg_medStore, login_medStore }
