01378b7d25fd98e150bc1668d5edc4a222e0cb12
Vivek Gupta <vivekvikigupta@gmail.com>
2022-11-20 00:11:07 +0530

    patient_pres_rec added

---
+++ controller/PatientCtrl/PatientPresRecord.js
@@ -0,0 +1,36 @@
+const prescription = require('../../model/prescriptionSchema')
+const patientSchema = require('../../model/patientSchema')
+
+const patient_pres_rec = async(req, res)=>{
+
+    const {name, role, uid} = req.userInfo;
+    console.log(name, role, uid)
+    const query_id = req.query.uid;
+    var pres_rec;
+
+    try {
+        if(role === 'doctor' && query_id){
+            var pres_rec = await prescription.find({health_id : query_id})
+        }
+        else if(role === 'patient'){
+            var pres_rec = await prescription.find({health_id : uid})
+        }
+        else{
+            throw Error("Unauthorised!")
+        }
+        
+        pres_rec = (pres_rec[0]).prescriptions.sort((a, b) =>b.date - a.date)
+        console.log(pres_rec);
+
+        return res
+                .status(200)
+                .json({prescriptions : pres_rec})
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({error : "Some Error occured"})
+    }
+
+}
+
+module.exports = {patient_pres_rec}
--- routes/data_routes.js
+++ routes/data_routes.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const add_prescriptions = require('../controller/prescription')
-const getAll_patients = require('../controller/getAll_patients.js')
-const {patient_pres_rec, patient_detail} = require('../controller/PatientCtrl/getPatientRecord')
+const {patient_pres_rec} = require('../controller/PatientCtrl/PatientPresRecord')
 const Authorize  = require('../middlewares/authorize')
 const bookappointment = require('../controller/PatientCtrl/bookappointment')
 const checkappointments = require('../controller/checkappointments')
-const patientProfile = require('../controller/patientprofile')
+
 const getdoctorslist  = require('../controller/PatientCtrl/getDoctorsList')
 const profile = require('../controller/common/profile')
 
@@ -14,11 +13,10 @@ const profile = require('../controller/common/profile')
 
 //imp note : to authorization for specific role(patient, doctor, pharmacy) to a specific route, just add middleware Authorize with arguments of array of roles
 //  For Example : router.get('/patient_pres_rec/:hid',Authorize(["doctor", "medical_store"]), patient_pres_rec )
-router.get('/getAll_patients', getAll_patients)
+
 
 //routes for patient
 router.post('/bookappointment', Authorize(["doctor", "patient", "pharmacy"]), bookappointment)
-router.get('/patientprofile', Authorize(["patient"]), patientProfile)
 router.get('/getdoctor',Authorize(["doctor", "patient", "pharmacy"]), getdoctorslist )
 
 //routes for doctors
@@ -27,8 +25,7 @@ router.post('/add_prescriptions', add_prescriptions)
 router.get('/chckappointments',Authorize(["doctor"]), checkappointments)
 
 //common routes
-router.get('/patient_pres_rec/:hid', Authorize(["doctor", "patient"]), patient_pres_rec )
-router.get('/patient_detail/:hid', Authorize(["doctor", "patient"]), patient_detail )
+router.get('/patient_pres_rec/', Authorize(["doctor", "patient"]), patient_pres_rec )
 router.get('/profile',Authorize(["doctor", "patient"]), profile )
 
 
